24f2e9639c9686b479cdb1f4d8824f1bfcfc4f93
francis <fc373745@gmail.com>
2019-04-09 10:07:15 -0700

    created image model

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_restful import Api
 from dotenv import load_dotenv
 from marshmallow import ValidationError
 from db import db
+from ma import ma
 
 
 app = Flask(__name__)
@@ -26,4 +27,5 @@ def handle_marshmallow_validation(err):
 
 if __name__ == "__main__":
     db.init_app(app)
+    ma.init_app(app)
     app.run(port=5000, debug=True)
