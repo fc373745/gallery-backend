28c9a11ad81319073bec37107cc098b04e4e9740
francis <fc373745@gmail.com>
2019-04-09 09:51:51 -0700

    finished initial app.py config

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Api
 from dotenv import load_dotenv
+from marshmallow import ValidationError
 from db import db
 
 
@@ -11,3 +12,18 @@ app.config.from_envvar(
     "APPLICATION_SETTINGS"
 )  # override with config.py (APPLICATION_SETTINGS points to config.py)
 api = Api(app)
+
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+
+@app.errorhandler(ValidationError)
+def handle_marshmallow_validation(err):
+    return jsonify(err.messages), 400
+
+
+if __name__ == "__main__":
+    db.init_app(app)
+    app.run(port=5000, debug=True)
