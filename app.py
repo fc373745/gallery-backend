fc302f6c0a1b662c203bacdc8981b1e445d459b0
francis <fc373745@gmail.com>
2019-04-09 09:40:34 -0700

    added sqlalch

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask
+from flask_restful import Api
 from dotenv import load_dotenv
+from db import db
 
 
 app = Flask(__name__)
 load_dotenv(".env", verbose=True)
+app.config.from_object("default_config")  # load default configs from default_config.py
+app.config.from_envvar(
+    "APPLICATION_SETTINGS"
+)  # override with config.py (APPLICATION_SETTINGS points to config.py)
+api = Api(app)
